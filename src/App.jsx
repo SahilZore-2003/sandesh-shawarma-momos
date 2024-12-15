2a30ab6b3f91a22efdf6ddceb2b73ac222d187cf
SahilZore-2003 <zoresahil80@gmail.com>
2024-12-15 10:15:50 +0530

    configure done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+   <div className='h-screen bg-neutral-700 text-4xl text-white grid place-items-center'>
+      Abhi Showarama Shop
+   </div>
+  )
+}
+
+export default App
