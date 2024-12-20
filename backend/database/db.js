97a2f7c3b64b80b444031ab1518636d6dca22483
SahilZore-2003 <zoresahil80@gmail.com>
2024-12-20 14:19:52 +0530

    fix some issue

--- backend/database/db.js
+++ backend/database/db.js
@@ -3,10 +3,10 @@ import { connect } from "mongoose";
 const connectToMongo = async () => {
 
   try {
-    await connect(`mongodb+srv://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@cluster0.2chhx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`);
+    await connect(`mongodb+srv://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@cluster0.yolmv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+`);
 
-    `mongodb+srv://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@cluster0.yolmv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
-`
+
 
     console.log("---***Database Connected Successfully***---")
   } catch (error) {
