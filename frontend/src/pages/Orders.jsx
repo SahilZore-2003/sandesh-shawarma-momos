8cc1460fd49e377b242aa06bb9c7158815fee6f6
SahilZore-2003 <zoresahil80@gmail.com>
2024-12-22 07:01:26 +0530

    order UI fixed

--- frontend/src/pages/Orders.jsx
+++ frontend/src/pages/Orders.jsx
@@ -3,16 +3,19 @@ import { IoMdListBox } from "react-icons/io";
 import { useToast } from "../hooks/use-toast"
 import { db } from "../firebase/firebase"
 import { getDoc, doc } from "firebase/firestore";
+import Loader from "../loaders/Loader";
 const Orders = () => {
     const { user } = JSON.parse(localStorage.getItem('user'))
     const { uid } = user;
     const { toast } = useToast()
     const [orders, setOrders] = useState([]);
-    console.log("🚀 ~ Orders ~ orders:", orders)
+    const [loading, setLoading] = useState(false);
+
 
 
     const fetchAllOrdersForUser = async (userId) => {
         try {
+            setLoading(true)
             // Reference to the document at orders/userId
             const userOrdersRef = doc(db, "orders", userId);
             const userOrdersSnap = await getDoc(userOrdersRef);
@@ -22,7 +25,10 @@ const Orders = () => {
                 const ordersArray = Object.entries(userData).map(([key, value]) => {
                     return { orderId: key, ...value };
                 });
-                setOrders(ordersArray)
+
+                const refactorOrder = ordersArray.map((data) => ({ ...data, ["order"]: Object.values(data.order) }))
+
+                setOrders(refactorOrder.sort((a, b) => new Date(b.date) - new Date(a.date)))
             } else {
                 toast({
                     title: "No orders availabel yet",
@@ -33,6 +39,8 @@ const Orders = () => {
             }
         } catch (error) {
             console.error("Error fetching user orders:", error);
+        } finally {
+            setLoading(false)
         }
     };
 
@@ -45,7 +53,14 @@ const Orders = () => {
     return (
         <div className="relative">
             {
-                orders?.length > 0 ? orders?.map(({
+                loading && <div className="min-h-[calc(100vh-92px)] grid place-items-center">
+                    <Loader size={50} />
+                </div>
+            }
+
+
+            {
+                !loading && orders?.length > 0 ? orders?.map(({
                     date, paymentMethod, order, totalBill
                 }, index) => (
                     <div key={index} className="flex gap-4 relative border-b-2 border-inputSecondary pb-2 pt-4 first:pt-0">
@@ -56,16 +71,21 @@ const Orders = () => {
                                 {date}
                             </p>
                             <div className="border-b-2 border-dashed border-black pb-2">
-                                <div className="mt-2 flex items-center justify-between gap-4 font-normal">
-                                    <p className="basis-full"> <span className="font-bold">{order[0].quntity}x</span>{order[0].name}</p>
-                                    <p className="basis-[20%] text-end shrink-0 font-bold">{order[0].price * order[0].quntity}</p>
-                                </div>
+                                {
+                                    order?.map((item, index) => (
+                                        <div key={index} className="mt-2 flex items-center justify-between gap-4 font-normal">
+                                            <p className="basis-full"> <span className="font-bold">{item?.quntity}x</span>{item?.name}</p>
+                                            <p className="basis-[20%] text-end shrink-0 font-bold">{item?.price * item?.quntity}</p>
+                                        </div>
+                                    ))
+                                }
+
 
                             </div>
                             <div className="border-b-2 border-dashed border-black py-2 font-bold">
                                 <div className="flex items-center justify-between">
                                     <p>subtotal</p>
-                                    <p>{order?.totalBill}</p>
+                                    <p>{totalBill}</p>
                                 </div>
                                 <div className="flex items-center justify-between">
                                     <p>Delivery Charges</p>
